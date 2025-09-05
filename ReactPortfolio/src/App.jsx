9bf24e3f0a3adbfea46c8455fce7f4aaac1c5ed3
FixMyCodeNow <153930601+FixMyCodeNow@users.noreply.github.com>
2025-09-05 10:48:51 +0700

    App.jsx

--- ReactPortfolio/src/App.jsx
+++ ReactPortfolio/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import Navbar from './components/navbar/navbar';
+import Navbar from './components/navbar/Navbar';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
